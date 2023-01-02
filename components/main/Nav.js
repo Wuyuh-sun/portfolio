cc38c7a975bc13a29e34a8c5c5acdd5ce9258f37
yunha <wjh0970@gmail.com>
2023-01-03 05:40:22 +0900

    navbar_bug_fix

--- components/main/Nav.js
+++ components/main/Nav.js
@@ -26,6 +26,8 @@ export default function Nav() {
   const navBg = useRef();
   const [iconArr, setIconArr] = useState([]);
 
+  const navBg_menuList = useRef();
+
   useEffect(() => {
     setIconArr([
       <>
@@ -67,103 +69,121 @@ export default function Nav() {
     !navState
       ? ((img.current.style.transform = "rotate(0deg)"),
         (navBg.current.style.height = "70px"),
-        (navBg.current.childNodes[0].style.opacity = "0"))
+        (navBg.current.childNodes[0].style.opacity = "0"),
+        setTimeout(() => {
+          navBg_menuList.current.childNodes.forEach((item, i) => {
+            item.style.display = "none";
+          });
+        }, 500))
       : ((img.current.style.transform = "rotate(180deg)"),
         (navBg.current.style.height = "calc(100% - 30px)"),
-        (navBg.current.childNodes[0].style.opacity = "1"));
+        (navBg.current.childNodes[0].style.opacity = "1"),
+        navBg_menuList.current.childNodes.forEach((item, i) => {
+          item.style.display = "flex";
+        }));
   }, [navState, navController]);
 
-  return ( iconArr &&
-    <nav>
-      <div
-        ref={img}
-        className={style.wrap}
-        onClick={() => {
-          !navState ? setNavState(true) : setNavState(false);
-        }}
-      >
-        <Image src={logoImg} width={50} height={50} alt="logoImg" loading="lazy"/>
-      </div>
-      <div ref={navBg} className={style.navBg}>
-        <div className={style.navBg_menuList}>
-          <div
-            onClick={() => {
-              navController.aboutme();
-              dispatch(nowLocate("aboutme"));
-            }}
-          >
-            {iconArr[0]}
-            <small
-              style={{
-                color: navController.now === "aboutme" ? "#4c94d4" : "black",
+  useEffect(() => {}, []);
+  return (
+    iconArr && (
+      <nav>
+        <div
+          ref={img}
+          className={style.wrap}
+          onClick={() => {
+            !navState ? setNavState(true) : setNavState(false);
+          }}
+        >
+          <Image
+            src={logoImg}
+            width={50}
+            height={50}
+            alt="logoImg"
+            loading="lazy"
+          />
+        </div>
+        <div ref={navBg} className={style.navBg}>
+          <div ref={navBg_menuList} className={style.navBg_menuList}>
+            <div
+              onClick={() => {
+                navController.aboutme();
+                dispatch(nowLocate("aboutme"));
               }}
             >
-              AboutMe
-            </small>
-          </div>
-          <div
-            onClick={() => {
-              navController.myinfo();
-              dispatch(nowLocate("myinfo"));
-            }}
-          >
-            {iconArr[1]}
-            <small
-              style={{
-                color: navController.now === "myinfo" ? "#4c94d4" : "black",
+              {iconArr[0]}
+              <small
+                style={{
+                  color: navController.now === "aboutme" ? "#4c94d4" : "black",
+                }}
+              >
+                AboutMe
+              </small>
+            </div>
+            <div
+              onClick={() => {
+                navController.myinfo();
+                dispatch(nowLocate("myinfo"));
               }}
             >
-              MyInfo
-            </small>
-          </div>
-          <div
-            onClick={() => {
-              navController.skills();
-              dispatch(nowLocate("skills"));
-            }}
-          >
-            {iconArr[2]}
-            <small
-              style={{
-                color: navController.now === "skills" ? "#4c94d4" : "black",
+              {iconArr[1]}
+              <small
+                style={{
+                  color: navController.now === "myinfo" ? "#4c94d4" : "black",
+                }}
+              >
+                MyInfo
+              </small>
+            </div>
+            <div
+              onClick={() => {
+                navController.skills();
+                dispatch(nowLocate("skills"));
               }}
             >
-              Skills
-            </small>
-          </div>
-          <div
-            onClick={() => {
-              navController.portfolio();
-              dispatch(nowLocate("portfolio"));
-            }}
-          >
-            {iconArr[3]}
-            <small
-              style={{
-                color: navController.now === "portfolio" ? "#4c94d4" : "black",
+              {iconArr[2]}
+              <small
+                style={{
+                  color: navController.now === "skills" ? "#4c94d4" : "black",
+                }}
+              >
+                Skills
+              </small>
+            </div>
+            <div
+              onClick={() => {
+                navController.portfolio();
+                dispatch(nowLocate("portfolio"));
               }}
             >
-              Portfolio
-            </small>
-          </div>
-          <div
-            onClick={() => {
-              // navController.more();
-              navController.more();
-              dispatch(nowLocate("more"));
-            }}
-          >
-            {iconArr[4]}
-            <small
+              {iconArr[3]}
+              <small
+                style={{
+                  color:
+                    navController.now === "portfolio" ? "#4c94d4" : "black",
+                }}
+              >
+                Portfolio
+              </small>
+            </div>
+            <div
+              onClick={() => {
+                // navController.more();
+                navController.more();
+                dispatch(nowLocate("more"));
+              }}
+            >
+              {iconArr[4]}
+              <small
               // style={{
               //   color: navController.now === "more" ? "#4c94d4" : "black",
               // }}
-            >
-              More
-            </small>
+              >
+                More
+              </small>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    )
   );
 }
